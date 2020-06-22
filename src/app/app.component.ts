6a4557f66d796ee7572dcf57d4f10a4beb18ac5d
Jesus Jimenez Cordero <jesus_heaven117@outlook.com>
2020-06-22 10:05:41 -0500

    feat: Add SASS mixins and variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
-import { Platform } from "@ionic/angular";
-import { Plugins } from "@capacitor/core";
+import { Platform } from '@ionic/angular';
+import { Plugins } from '@capacitor/core';
 
 const { StatusBar } = Plugins;
 
 @Component({
-  selector: "app-root",
-  templateUrl: "app.component.html",
-  styleUrls: ["app.component.scss"],
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
   constructor(private platform: Platform) {
